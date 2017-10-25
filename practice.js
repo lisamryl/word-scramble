fb7614edc2d7f3636ed58fd2ce57e496241655c7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-25 15:28:39 -0700

    browser game works

--- practice.js
+++ practice.js
@@ -1,27 +1,32 @@
-
+"use strict";
 
 function swapLetters(wordList) {
     //randomly generating a number in range of length of wordList
+    //floor rounds down all decimals
     let firstIndex = Math.floor(Math.random() * wordList.length);
     let secondIndex = Math.floor(Math.random() * wordList.length);
 
-    let returnList = wordList;
+//logic of swap:
+//a to temp, temp to b, b to a
+    let temp = wordList[secondIndex];
+    wordList[secondIndex] = wordList[firstIndex];
+    wordList[firstIndex] = wordList[temp];
 
-    returnList[firstIndex] = wordList[secondIndex];
-    returnList[secondIndex] = wordList[firstIndex];
-
-    return returnList;
+    // return wordList;
 }
 
 function printCandyMap(color, candyMap) { 
     if (candyMap.has(color)) {
         return candyMap.get(color);
     } else {
-        console.log("Sorry, that color doesn't have a flavor, and I'm not
-            printing your typo.");
+        console.log("Sorry, that color doesn't have a flavor, and I'm not printing your typo.");
     }
 }
 
 //horrifyingly ugly thing that can be pasted into console
-let candyMap = new Map ([['purple', 'grape'], ['blue', 'blueberry'], ['red', 'red flavor'], ['yellow', 'lemon'], ['green', 'lime' ]])
-function printCandyMap(color, candyMap) { if (candyMap.has(color)) {return candyMap.get(color);} else {console.log("Sorry, that color doesn't have a flavor, and I'm not printing your typo.");}}
+let candyMap = new Map ([['purple', 'grape'], 
+                         ['blue', 'blueberry'],
+                         ['red', 'red flavor'],
+                         ['yellow', 'lemon'],
+                         ['green', 'lime' ]]);
+// function printCandyMap(color, candyMap) { if (candyMap.has(color)) {return candyMap.get(color);} else {console.log("Sorry, that color doesn't have a flavor, and I'm not printing your typo.");}}
--- scramble-helper.js
+++ scramble-helper.js
@@ -1,9 +1,31 @@
 // Create a function that reverses a word
+"use strict";
+
+let reverseWord = function (aString) {
+
+    let newWord = '';
+    for (let i = aString.length -1; i >= 0; i -=1 ) {
+        newWord += aString.charAt(i);
+    }
+    return newWord;
+
+};
 
 // Create a function that takes an array of words and returns a new array of the
  // words with each word reversed.
 
+let reversedWords = function (wordArrays) {
+
+//.map is way of for looping
+
+    wordArrays.map(reverseWord);
+//curly brace for function, semi colon is for let =
+ };
+
+
 // Create a function that returns a random word from an array
+
+
 
 // Create an array of words and save it to a variable. Using your functions create
 // a second array of reversed words.
@@ -34,3 +56,66 @@
 
 // Create a function that takes an array of words and returns a map with the
 // scrambled words as the keys and the original word as the values.
+
+"use strict";
+
+// Create a function that reverses a word
+
+// Create a function that returns a random word from an array
+
+function randomWord(words) {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+const words = ["strawberry", "stitch", "indecipherable", "donut"];
+
+const reveresedWords = reversedWords(words);
+
+// Create a function that takes an array of words and returns a map with the
+// keys as the reversed word and the values as the original word.
+
+
+function reverseWordAnswers(words) {
+  let wordReverse = new Map();
+  for (let word of words) {
+    wordReverse.set(reverseWord(word), word);
+  }
+  return wordReverse;
+}
+
+// Create a function that takes two strings and a map, like the one you created
+// above. If the first string matches the value of the scrambled word in the map,
+// return true, otherwise return false
+
+function checkWord(guess, scrambledWord, wordMap) {
+  if (wordMap.get(scrambledWord) === guess) {
+    return true;
+  } else {
+    return false;
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+// FURTHER STUDY
+
+function scramble(str) {
+    return str.split('').sort(function()
+        {
+            return 0.5-Math.random()
+        }).join('');
+}
+
+function scrambleWordAnswers(words) {
+  let wordReverse = new Map();
+  for (let word of words) {
+    wordReverse.set(scramble(word), word);
+  }
+  return wordReverse;
+}
+
+function scrambleWords(words) {
+  return words.map(scramble);
+}
+
+function getRandomScramble(wordMap) {
+  return randomWord(Array.from(wordMap.keys()));
+}
